dc6f89bb69c152ef07c394fbf6d548d6f806ff28
apple <tmgf.15.su@gmail.com>
2019-11-08 10:06:12 +0900

    enth seg

--- scripts/runs/191106_enth_seg.py
+++ scripts/runs/191106_enth_seg.py
@@ -17,6 +17,6 @@ if __name__ == '__main__':
     runner = Runner(RUN_NAME, get_model_cls(config["model_name"]),\
         config["features"], config["target"], config["hyper_params"], config["loss"])
 
-    runner.run_train_cv()
-    runner.run_predict_cv()
+    # runner.run_train_cv()
+    # runner.run_predict_cv()
     runner.create_submission()
